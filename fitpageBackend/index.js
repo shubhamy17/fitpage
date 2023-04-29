f975359d6c3a3c859eda5a23c6ba22a24d4817ed
Shubham yadav <shubhamyadav7303@gmail.com>
2023-04-29 15:05:48 +0530

    comment added backend

--- fitpageBackend/index.js
+++ fitpageBackend/index.js
@@ -1,17 +1,22 @@
+// Import required modules
 const express = require('express');
 const cors = require('cors');
-const data = require('./data.json');
-
+const data = require('./data.json');  // Import data from JSON file
+// Initialize express app
 const app = express();
 
+// Enable CORS middleware
 app.use(cors());
 
+// Define endpoint for getting data
 app.get('/data', (req, res) => {
-  res.json(data);
+  res.json(data); // Send the data as JSON response
 });
 
+// Set the port to either the environment variable or 5000
 const port = process.env.PORT || 5000;
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
